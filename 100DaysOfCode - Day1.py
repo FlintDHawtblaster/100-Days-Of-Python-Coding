4e6c909fb4b2ae7fe7e95759ed06d775d619cc0f
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-02-03 14:17:53 +0000

    Initial commit

---
+++ 100DaysOfCode - Day1.py
@@ -0,0 +1,13 @@
+print(""" 
+Danny Flint Junior 
+December 12th 2024
+
+I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day.
+I'll be using VS Code, an amazing IDE fo the coding challenges.
+No more excuses!
+
+I'm feeling 🔥
+
+""")
