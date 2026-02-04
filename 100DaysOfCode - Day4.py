91733d88acf19fec38587c864b5512ad61181c74
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-02-04 22:15:47 +0000

    Added Day 4

---
+++ 100DaysOfCode - Day4.py
@@ -0,0 +1,12 @@
+print("=== Your Adventure Simulator ===")
+print("""You'll  be asked a bunch of questions
+then we'll make you up an amazing 
+story with YOU as the star! 🤩""")
+print()
+name = input("Your name: ")
+enemy = input("Your worst enemy's name: ")
+superPower = input("Your super power: ")
+print()
+print("Our story begins as our hero name approaches a foreboding castle...")
+print("Suddenly, a bolt of lightning strikes the ground at the feet of","\033[33m", name,"\033[0m",)
+print("'Our final battle begins!' shouts the evil","\033[31m", enemy,"\033[0m", "clearly missing the fact that","\033[33m", name,"\033[0m", "has the power of","\033[35m", superPower,"\033[0m", "which means they'll win quite easily")
