586cb753b71cd520de515558e0447733965b43c9
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-02-10 15:36:35 +0000

    Adding Day 20

---
+++ 100DaysOfCode - Day20.py
@@ -0,0 +1,10 @@
+print("List Generator")
+print()
+print()
+initial = int(input("Start at: "))
+ending = int(input("End before: "))
+incre = int(input("Increment between values: "))
+print()
+for i in range(initial, ending, incre):
+    print( i )
+
