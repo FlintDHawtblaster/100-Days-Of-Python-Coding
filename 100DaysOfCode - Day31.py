03049f95e67b2f27126d1da0d2ba4a44d81cd018
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-02-16 20:43:03 +0000

    Day 31 Added

---
+++ 100DaysOfCode - Day31.py
@@ -0,0 +1,49 @@
+print("\033[33m")
+title = "🔴⚪🔵  Music App 🔴⚪🔵"
+print(f"{title:^45}")
+print()
+print("🔥▶️", end="    ")
+print("\033[0m", end="")
+print("Radio Gaga")
+print("\033[33m", end="")
+queen = "Queen"
+print(f"{queen:^20}")
+print("\033[m")
+print()
+prev = "PREV"
+next = "NEXT"
+pause = "PAUSE"
+print(f"{prev}", end="")
+print("\033[32m")
+print(f"{next:^15}", end="")
+print("\033[31m")
+print(f"{pause:^25}", end="")
+print("\033[0m")
+print()
+print()
+print()
+print()
+print()
+print()
+title2 = "WELCOME TO"
+sub = "--        ARMBOOK        --"
+print(f"{title2:^45}")
+print("\033[34m", end="")
+print(f"{sub:^45}")
+print("\033[33m")
+firstLine = "Definitely not a rip off of" 
+secondLine = "a certain other social"
+thirdLine = "networking site"
+fourth = "Honest."
+print(f"{firstLine:>40}")
+print(f"{secondLine:>40}")
+print(f"{thirdLine:>40}")
+print("\033[31m")
+print(f"{fourth:^45}")
+print("\033[0m")
+username = "Username:"
+password = "Password:"
+print(f"{username:^45}")
+print(f"{password:^45}")
+
+
