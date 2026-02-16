bd8152b8b6db2268a50fde56306359618172d4da
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-02-16 20:43:29 +0000

    Day 32 Added

---
+++ 100DaysOfCode - Day32.py
@@ -0,0 +1,6 @@
+from random import randint
+
+greetings = ["Hello there!", "Konnichiwa!", "Guten Tag!", "Bore Da!", "Bonjour!", "Hola!", "Salve!"]
+
+print(f"{greetings[randint(0,6)]} to you guys")
+
