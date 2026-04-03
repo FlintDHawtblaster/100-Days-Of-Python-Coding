d3ee8a72aa9b9f6894c5fa2f523ae4c3de9bf9af
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-04-03 21:46:35 +0000

    Solution for Day 53 challenge

---
+++ 100DaysOfCode, solution - Day53.py
@@ -0,0 +1,70 @@
+import os, time
+
+inventory = []
+
+try:
+    f = open("inventorys.txt", "r")
+    inventory = eval(f.read())
+    f.close()
+except:
+    pass
+
+def addItem():
+    time.sleep(1)
+    os.system("cls")
+    print("INVENTORY")
+    print("=========")
+    print()
+    item = input("Item to add > ").capitalize()
+    inventory.append(item)
+    print("Added")
+    
+def viewItem():
+    os.system("cls")
+    print("INVENTORY")
+    print("=========")
+    print()
+    seen = set(inventory)
+    for item in seen:
+        print(f"{item} {inventory.count(item)}")
+        
+    #Alternative
+    #seen = []
+    #for item in inventory:
+    #    if item not in seen: 
+    #        print(f"{item} {inventory.count(item)}")
+    #        seen.append(item)
+        
+    time.sleep(2)
+    
+def removeItem():
+    time.sleep(1)
+    os.system("cls")
+    print("INVENTORY")
+    print("=========")
+    print()
+    item = input("Item to remove > ").capitalize()
+    if item in inventory:
+        inventory.remove(item)
+        print("Removed")
+    else:
+        print("You don't have that item")
+
+while True:
+    time.sleep(1)
+    os.system("cls")
+    print("INVENTORY")
+    print("=========")
+    print()
+    
+    menu = input("1: Add\n2: View\n3: Remove\n\n> ")
+    if menu == "1":
+        addItem()
+    elif menu == "2":
+        viewItem()
+    else:
+        removeItem()
+        
+    f = open("inventorys.txt", "w")
+    f.write(str(inventory))
+    f.close()
