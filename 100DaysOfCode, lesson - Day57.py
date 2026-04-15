78326c4cbb714d50e239cdb831e62068ba18e9f1
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-04-15 22:02:29 +0000

    Lesson on recursion

---
+++ 100DaysOfCode, lesson - Day57.py
@@ -0,0 +1,11 @@
+def reverse(value):
+    if value <= 0:
+        print("Done!")
+        return
+    else:
+        for i in range(value):
+            print("💯", end="")
+        print()
+        reverse(value-1)
+        
+reverse(10)
