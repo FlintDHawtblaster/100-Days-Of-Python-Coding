6d9cb455e02577676bbd3a8ad452300e6d305da9
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-05-04 22:51:47 +0000

    Add Replit DB but only works in replit

---
+++ 100DaysOfCode - Day61.py
@@ -0,0 +1,37 @@
+#I need to import from replit
+from replit import db
+import datetime, os, time
+
+def addTweet():
+    tweet = input("🐦 > ")
+    timestamp = datetime.datetime.now()
+    key = f"mes{timestamp}"
+    db[key] = tweet
+    time.sleep()
+    os.system("cls")
+    
+def viewTweet():
+    matches = db.prefix("mes")
+    matches = matches[::-1]
+    counter = 0
+    for i in matches:
+        print(db[i])
+        print()
+        time.sleep(0.5)
+        counter+=1
+        if (counter%10 == 0):
+            carryOn = input("Next 10?: ")
+            if (carryOn.lower() == "no"):
+                break
+    time.sleep(2)
+    os.system("cls")
+    
+
+while True:
+    print("Tweeter")
+    menu = input("1: Add Tweet\n2: View Tweets\n> ")
+    
+    if menu == "1":
+        addTweet()
+    else:
+        viewTweet()
