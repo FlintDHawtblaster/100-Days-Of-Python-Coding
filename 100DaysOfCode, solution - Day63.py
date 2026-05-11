56b5ae252b45da3004535efca527b6679a0377e4
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-05-11 13:45:50 +0000

    Use functions from a different python file

---
+++ 100DaysOfCode, solution - Day63.py
@@ -0,0 +1,10 @@
+import coolSubroutines as cs
+
+cs.newPrint("red")
+print("This should be red")
+cs.newPrint("blue")
+print("This should be blue")
+cs.newPrint("green")
+print("This should be green")
+cs.newPrint("white")
+print("This should be default")
