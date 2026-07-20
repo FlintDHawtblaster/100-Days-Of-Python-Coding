7daa0e873a4c2c7f0f6d9ac6c82e6ce665334f3c
FlintDHawtblaster <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-07-20 20:55:08 +0000

    Example python file

---
+++ ex.py
@@ -0,0 +1,11 @@
+green = False
+small = True
+
+if not small and green:
+    print("cherry")
+elif green and small:
+    print("pea")
+elif not small and not green:
+    print("watermelon")
+else:
+    print("I don't know")
